6d37e131b00d17ff4b92ca023f29b506b127f27c
Gabriele Mangione <105911738+Gabriele-Mangione@users.noreply.github.com>
2024-02-23 14:37:44 +0100

    added commentary to remap

--- lua/gab/remap.lua
+++ lua/gab/remap.lua
@@ -1,9 +1,14 @@
 vim.g.mapleader = " "
+--exit file
 vim.keymap.set("n","<leader><esc>",vim.cmd.Ex)
+--save file
 vim.keymap.set("n","<leader>s",":w<CR>")
 vim.lsp.buf.format()
-vim.keymap.set("n","<leader>y","\"+y") --somehow doesn't work, maybe cause windows
+--copy into clipboard
+vim.keymap.set("n","<leader>y","\"+y")
 vim.keymap.set("v","<leader>y","\"+y")
 
+--remove "Q" operator
 vim.keymap.set("n","Q", "<nop>")
+--format file
 vim.keymap.set("n","<leader>f", vim.lsp.buf.format)
